5f07d13cacd8ee77bebf00aee4efe2e5a9f555ed
nlmanama <nlminhanh12@gmail.com>
2023-03-05 18:47:22 -0800

    Added JSONWriter class

---
+++ src/main/persistence/JsonWriter.java
@@ -0,0 +1,46 @@
+package persistence;
+
+import model.Library;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class JsonWriter {
+    private PrintWriter writer;
+    private String destination;
+
+    // EFFECTS: create a writer to write to given destination
+    public JsonWriter(String destination) {
+        this.destination = destination;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer to write to a new file created at destination, throws FileNotFound if destination file
+    // cannot be opened for writing
+    public void open() throws FileNotFoundException {
+        writer = new PrintWriter(new File(destination));
+    }
+
+    // EFFECTS: writes JSON representation of library to file
+    public void write(Library lib) {
+        JSONObject json = lib.toJson();
+        writeToFile(json.toString());
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+        writer.close();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void writeToFile(String json) {
+        writer.print(json);
+    }
+
+
+
+}
