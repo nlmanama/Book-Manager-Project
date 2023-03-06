4e8f56abfa29037d0a12a68b7e43ce11ea91f572
nlmanama <nlminhanh12@gmail.com>
2023-03-05 21:09:51 -0800

    Added file in data

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -7,6 +7,7 @@ import java.io.FileNotFoundException;
 
 public class Main {
 
+    // EFFECTS: run the library application
     public static void main(String[] args) {
         try {
             new LibraryApp();
