a595c12fb9136e308ab6221034f59fc137064f92
nlmanama <nlminhanh12@gmail.com>
2023-03-27 02:19:41 -0700

    Better message

--- src/main/ui/AddBookFrame.java
+++ src/main/ui/AddBookFrame.java
@@ -7,6 +7,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+// A window that allows features of a book to be input and adds the book to library
 public class AddBookFrame {
 
     private JFrame frame;
