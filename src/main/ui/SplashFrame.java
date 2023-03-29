e4ba38a590923296d67ad14ea045929b4f39e001
nlmanama <nlminhanh12@gmail.com>
2023-03-29 02:59:26 -0700

    Documented SplashArt

--- src/main/ui/SplashFrame.java
+++ src/main/ui/SplashFrame.java
@@ -3,14 +3,18 @@ package ui;
 import javax.swing.*;
 import java.awt.*;
 
+// The splash screen when the application starts
 public class SplashFrame {
 
     private JFrame frame;
 
+    // EFFECTS: create the splash screen
     public SplashFrame() {
         initialize();
     }
 
+    // MODIFIES: this
+    // EFFECTS: create the frame with the SplashArt file as the full screen
     private void initialize() {
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,6 +31,8 @@ public class SplashFrame {
         frame.setVisible(true);
     }
 
+    // MODIFIES: this
+    // EFFECTS: hides the frame
     public void hide() {
         frame.setVisible(false);
     }
