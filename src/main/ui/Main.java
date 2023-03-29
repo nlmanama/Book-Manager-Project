4120cd4ec3e68762727e978eba32a91c0b4e9b5f
nlmanama <nlminhanh12@gmail.com>
2023-03-29 02:36:42 -0700

    Changed main comment

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 
 public class Main {
 
-    // EFFECTS: run the library application
+    // EFFECTS: run the application
     public static void main(String[] args) {
 //        try {
 //            new LibraryApp();
